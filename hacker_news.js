279213f567cc675ec4662e459f8f60fb0939addc
DataFire <info@datafire.io>
2017-11-07 16:56:33 -0600

    test3

--- hacker_news.js
+++ hacker_news.js
@@ -2,6 +2,8 @@
 let datafire = require('datafire');
 
 let hacker_news = require('@datafire/hacker_news').actions;
+let google_sheets = require('@datafire/google_sheets').actions;
+let project = require('@datafire/project').actions;
 module.exports = new datafire.Action({
   handler: async (input, context) => {
     let storyIDs = await hacker_news.getStories({
@@ -10,6 +12,7 @@ module.exports = new datafire.Action({
     let item = await Promise.all(storyIDs.map(storyID => hacker_news.getItem({
       itemID: storyID,
     }, context)));
-    return item;
+    let user = await hacker_news.getUser({}, context);
+    return user;
   },
 });
