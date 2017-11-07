24fb72bc6c534591385c004f9a25fb4923165061
DataFire <info@datafire.io>
2017-11-07 16:49:10 -0600

    test2

--- hacker_news.js
+++ hacker_news.js
@@ -1,8 +1,15 @@
 "use strict";
 let datafire = require('datafire');
 
+let hacker_news = require('@datafire/hacker_news').actions;
 module.exports = new datafire.Action({
   handler: async (input, context) => {
-
+    let storyIDs = await hacker_news.getStories({
+      storyType: "top",
+    }, context);
+    let item = await Promise.all(storyIDs.map(storyID => hacker_news.getItem({
+      itemID: storyID,
+    }, context)));
+    return item;
   },
 });
